335b5b2b6a849ce6928b63437eb6c04539fbc090
Randall Brown <blladnar@gmail.com>
2022-04-20 13:12:47 -0700

    Initial Commit

---
+++ HikerJournal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HikerJournal
+//
+//  Created by Randall Brown on 4/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HikerJournal/HikerJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikerJournalApp.swift
+//  HikerJournal
+//
+//  Created by Randall Brown on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct HikerJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
